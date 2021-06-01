e3299201b4bf3c730569dddce095c5148996a589
jackfrost0 <jackfrost0@ksu.edu>
2021-05-31 20:25:50 -0500

    Add project files.

---
+++ MisinformationApp/MisinformationApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MisinformationApp.Services;
+using MisinformationApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MisinformationApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
